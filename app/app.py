aedb59e0732adb202f57452289e1f9c3e6821865
s-omarali <syedomarali03@gmail.com>
2025-05-08 22:23:31 -0500

    fixing auth path

--- app/app.py
+++ app/app.py
@@ -1,15 +1,34 @@
 from flask import Flask
 from routes.listing_routes import listing_bp
 from routes.bid_routes import bid_bp
+from routes.auth import auth_bp  # Import the auth Blueprint
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
 
-app = Flask(__name__)
-app.register_blueprint(listing_bp)
-app.register_blueprint(bid_bp)
-@app.route('/')
-def home():
-    return "Bidding App API is running!"
+load_dotenv()
 
+def create_app():
+    app = Flask(__name__)
+
+    
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
+
+
+    jwt = JWTManager(app)
+
+    # Register Blueprints
+    app.register_blueprint(listing_bp, url_prefix='/listings')
+    app.register_blueprint(bid_bp, url_prefix='/bids')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    @app.route('/')
+    def home():
+        return "Bidding App API is running!"
+
+    return app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
-
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
